c8e845a936fbe60b7011171069bb48b37c673dcd
Yahton <>
2024-02-09 19:32:57 +0300

    refactoring (thanks to vivirinter)

--- main.py
+++ main.py
@@ -1,134 +1,73 @@
 import math
+import re
 
 
-VALID_CHARACTERS = set(" 0123456789+-*/()^sin.co")
+VALID_CHARACTERS = set(" 0123456789+-*/()^sinco")
 
+OPERATIONS = {
+    '+': (lambda a, b: a + b, 2, 1),
+    '-': (lambda a, b: a - b, 2, 1),
+    '*': (lambda a, b: a * b, 2, 2),
+    '/': (lambda a, b: a / b, 2, 2),
+    '^': (lambda a, b: a ** b, 2, 3),
+    'sin': (lambda a: math.sin(a), 1, 4),
+    'cos': (lambda a: math.cos(a), 1, 4),
+}
 
 def add_spaces(s: str) -> str:
-    i = 0
-    while i < len(s):
-        if s[i] in '+-*/)^':
-            if s[i-1] != ' ':
-                s = s[:i] + ' ' + s[i:]
-                i += 1
-                
-        if s[i] in '+*/(^':
-            if s[i+1] != ' ':
-                s = s[:i+1] + ' ' + s[i+1:]
-                i += 1
-        if s[i:i+3] in ('sin', 'cos'):
-            if s[i-1] != ' ':
-                s = s[:i] + ' ' + s[i:]
-                i += 1
-            if s[i+3] != ' ':
-                s = s[:i+3] + ' ' + s[i+3:]
-                i += 1
-        i += 1
+    #  using regular expressions to add spaces between operation characters
+    s = re.sub(r'(\+|-|\*|/|\^|\(|\))', r' \1 ', s)
+    s = re.sub(r'(sin|cos)', r' \1 ', s)
     return s
 
-def exp_eval(exp: list) -> int:  # exp - expression
-    i = 0
-    while i < len(exp):
-        if exp[i] == 'sin':
-            exp[i] = math.sin(exp[i+1])
-            exp.pop(i+1)
-            i = 0
-            continue
-        if exp[i] == 'cos':
-            exp[i] = math.cos(exp[i+1])
-            exp.pop(i+1)
-            i = 0
-            continue
-        i += 1
-
-    i = 0
-    while i < len(exp):
-        if exp[i] == '^':
-            exp[i] = exp[i-1] ** exp[i+1]
-            exp.pop(i-1)
-            exp.pop(i)
-            i = 0
-            continue
-        i += 1
-    i = 0
-    while i < len(exp):
-        if exp[i] == '*':
-            exp[i] = exp[i-1] * exp[i+1]
-            exp.pop(i-1)
-            exp.pop(i)
-            i = 0
-            continue
-        if exp[i] == '/':
-            exp[i] = exp[i-1] / exp[i+1]
-            exp.pop(i-1)
-            exp.pop(i)
-            i = 0
-            continue
-        i += 1
-    i = 0
-    while i < len(exp):
-        if exp[i] == '+':
-            exp[i] = exp[i-1] + exp[i+1]
-            exp.pop(i-1)
-            exp.pop(i)
-            i = 0
-            continue
-        elif exp[i] == '-':
-            exp[i] = exp[i-1] - exp[i+1]
-            exp.pop(i-1)
-            exp.pop(i)
-            i = 0
-            continue
-        i += 1
-    return exp
-
-def metacalc(exp: list) -> int|float:  # exp - expression
-    while len(exp) != 1:
-        if '(' in exp:
-            ioc = 0
-            while True:
-                io = exp.index('(', ioc)
-                ic = exp.index(')')
-                if '(' in exp[io+1:ic]:
-                    ioc += 1
-                    continue
-                break
-            exp[io:ic+1] = metacalc(exp[io+1:ic])
+def evaluate_rpn(expression: list) -> float:
+    stack = []
+    for token in expression:
+        if token in OPERATIONS:
+            #  popping the required number of arguments onto the stack
+            args = [stack.pop() for _ in range(OPERATIONS[token][1])]
+            #  applying the operation and appending the result back to the stack
+            stack.append(OPERATIONS[token][0](*reversed(args)))
         else:
-            exp = exp_eval(exp)
+            stack.append(float(token))
+    return stack.pop()
 
-    return exp
+def infix_to_rpn(expression):
+    output = []
+    stack = []
+    
+    for token in expression:
+        if token.isdigit():
+            output.append(token)
+        elif token in OPERATIONS:
+            while stack and stack[-1] != '(' and OPERATIONS[token][2] <= OPERATIONS.get(stack[-1], [0, 0, 0])[2]:
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+    
+    while stack:
+        output.append(stack.pop())
+    
+    return output
 
-def main(inp: str) -> int|float:
-    if inp == "":
+def main(string: str) -> int|float:
+    if string == "":
         return 0
-    if not VALID_CHARACTERS.issuperset(inp):
-        raise SyntaxError("Inputted string contains invalid symbols!")
+    if not VALID_CHARACTERS.issuperset(string):
+        raise SyntaxError("The entered string contains invalid characters!")
 
-    inp = add_spaces(inp)
-    inp = inp.split()
+    exp = add_spaces(string).split()
+    exp = infix_to_rpn(exp)
+    result = evaluate_rpn(exp)
 
-    i = 0
-    while i < len(inp):
-        try:
-            inp[i] = float(inp[i])
-            if inp[i] < 0 and isinstance(inp[i-1], int|float):
-                inp.insert(i, '+')
-                i += 1
-            if inp[i] < 0 and inp[i-1] == ')':
-                inp.insert(i, '+')
-                i += 1
-        except ValueError:
-            pass
-        i += 1
-
-    if inp.count('(') != inp.count(')'):
-        raise SyntaxError("number of '(' is not equals to number of ')'")
-
-    return metacalc(inp)[0]
+    return result
 
 if __name__ == '__main__':
-    print("simplecalc v1.1 python edition")
+    print("simplecalc v1 python edition")
     while True:
-        inp = input(">>> ")
-        print(main(inp))
+        print(main(input(">>> ")))

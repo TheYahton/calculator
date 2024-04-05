5162fe6ae686419a51e4404af3274485e64bbef5
Yahton <>
2024-04-05 09:10:56 +0300

    spring update UwU

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import math
 import re
+import operator as op
 
 
-VALID_CHARACTERS = set(" 0123456789+-*/()^.>==<%E sin cos tan cot sqrt ln pi e 0x 0b 0o ABCDEF abcdef j")
+VALID_CHARACTERS = set(
+    " 0123456789+-*/()^.>==<%E sin cos tan cot sqrt ln pi e 0x 0b 0o ABCDEF abcdef j"
+)
 
 NUMBER_SYSTEMS = {
-    '0b': 2,
-    '0o': 8,
-    '0x': 16,
+    "0b": 2,
+    "0o": 8,
+    "0x": 16,
 }
 
 OPERATIONS = {
-    '==': (lambda a, b: a == b, 2, 0),
-    '>': (lambda a, b: a > b, 2, 0),
-    '<': (lambda a, b: a < b, 2, 0),
-    '>=': (lambda a, b: a >= b, 2, 0),
-    '<=': (lambda a, b: a <= b, 2, 0),
-    '+': (lambda a, b: a + b, 2, 1),
-    '-': (lambda a, b: a - b, 2, 1),
-    '*': (lambda a, b: a * b, 2, 2),
-    '/': (lambda a, b: a / b if b != 0 else math.inf, 2, 2),
-    '//': (lambda a, b: a // b, 2, 2),
-    '%': (lambda a, b: a % b, 2, 2),
-    '<<': (lambda a, b: a << b, 2, 2),
-    '>>': (lambda a, b: a >> b, 2, 2),
-    '^': (lambda a, b: a ^ b, 2, 3),
-    '**': (pow, 2, 3),
-    'sin': (math.sin, 1, 4),
-    'cos': (math.cos, 1, 4),
-    'tan': (math.tan, 1, 4),
-    'cot': (lambda a: 1 / math.tan(a), 1, 4),
-    'sqrt': (math.sqrt, 1, 4),
-    'ln': (math.log, 1, 4),
+    "sqrt": (math.sqrt, 1, 4),
+    "sin": (math.sin, 1, 4),
+    "cos": (math.cos, 1, 4),
+    "tan": (math.tan, 1, 4),
+    "cot": (lambda a: 1 / math.tan(a), 1, 4),
+    "ln": (math.log, 1, 4),
+    "==": (op.eq, 2, 0),
+    "**": (op.pow, 2, 3),
+    "//": (op.floordiv, 2, 2),
+    "<<": (op.lshift, 2, 2),
+    ">>": (op.rshift, 2, 2),
+    ">=": (op.ge, 2, 0),
+    "<=": (op.le, 2, 0),
+    ">": (op.gt, 2, 0),
+    "<": (op.lt, 2, 0),
+    "+": (op.add, 2, 1),
+    "-": (op.sub, 2, 1),
+    "*": (op.mul, 2, 2),
+    "/": (op.truediv, 2, 2),
+    "%": (op.mod, 2, 2),
+    "^": (op.xor, 2, 3),
 }
 
 
@@ -48,23 +51,61 @@ def str2digit(s: str) -> int | float | str:
                 return s
 
 
-def tokenize(s: str) -> list:
-    # using regular expressions to add spaces between operation characters
-    s = re.sub(r'(//|>=|<=|\*\*|>>|<<|==|\+|-|\*|/|\^|\(|\)|>|<|%)', r' \1 ', s)
-    s = re.sub(r'(sin|cos|tg|ctg|sqrt|ln)', r' \1 ', s)
+def sanitaze(string: str) -> str:
+    for symbol in "+*^()":
+        string = string.replace(symbol, "\\" + symbol)
+    return string
 
-    expression = s.split()
+
+def tokenize(string: str) -> list[str | int | float | complex]:
+    pattern = "(" + sanitaze("|".join(OPERATIONS) + "|)|(") + ")"
+    # using regular expressions to add spaces between operation characters
+    string = re.sub(pattern, r" \1 ", string)
+
+    expression = string.split()
     expression = list(map(str2digit, expression))
 
     for i, val in enumerate(expression):
-        if val == '-' and (not isinstance(expression[i - 1], (int, float)) or i == 0):
+        if (
+            val == "-"
+            and (not isinstance(expression[i - 1], (int, float)) or i == 0)
+            and not expression[i - 1] == ")"
+        ):
             expression[i + 1] = -expression[i + 1]
             expression.pop(i)
 
     return expression
 
 
-def evaluate_rpn(expression: list) -> float:
+def infix2rpn(expression) -> list[str | int | float | complex]:
+    output = []
+    stack = []
+
+    for token in expression:
+        if isinstance(token, (int, float, complex)):
+            output.append(token)
+        elif token in OPERATIONS:
+            while (
+                stack
+                and stack[-1] != "("
+                and OPERATIONS[token][2] <= OPERATIONS.get(stack[-1], [0, 0, 0])[2]
+            ):
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == "(":
+            stack.append(token)
+        elif token == ")":
+            while stack and stack[-1] != "(":
+                output.append(stack.pop())
+            stack.pop()
+
+    while stack:
+        output.append(stack.pop())
+
+    return output
+
+
+def evaluate_rpn(expression: list) -> int | float:
     stack = []
     for token in expression:
         if token in OPERATIONS:
@@ -77,44 +118,20 @@ def evaluate_rpn(expression: list) -> float:
     return stack.pop()
 
 
-def infix_to_rpn(expression) -> list:
-    output = []
-    stack = []
-
-    for token in expression:
-        if isinstance(token, (int, float, complex)):
-            output.append(token)
-        elif token in OPERATIONS:
-            while stack and stack[-1] != '(' and OPERATIONS[token][2] <= OPERATIONS.get(stack[-1], [0, 0, 0])[2]:
-                output.append(stack.pop())
-            stack.append(token)
-        elif token == '(':
-            stack.append(token)
-        elif token == ')':
-            while stack and stack[-1] != '(':
-                output.append(stack.pop())
-            stack.pop()
-
-    while stack:
-        output.append(stack.pop())
-
-    return output
-
-
 def calculate(string: str) -> float:
     if string == "":
         return 0
     if not VALID_CHARACTERS.issuperset(string):
         raise SyntaxError("The entered string contains invalid characters!")
 
-    exp = tokenize(string)
-    exp = infix_to_rpn(exp)
-    result = evaluate_rpn(exp)
+    expression = tokenize(string)
+    expression = infix2rpn(expression)
+    result = evaluate_rpn(expression)
 
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("serpentinum calculatio")
     while True:
         print(calculate(input(">>> ")))
--- tests.py
+++ tests.py
@@ -1,32 +1,49 @@
 import unittest
-from main import calculate, tokenize, evaluate_rpn, infix_to_rpn
+from main import str2digit, sanitaze, tokenize, infix2rpn, evaluate_rpn, calculate
 from math import inf, sin
 
+STR2DIGIT_TEST = (("5", 5), ("-6", -6), ("3.65", 3.65), ("3+5.5j", 3 + 5.5j))
+
+SANITAZE_TEST = (("5+3", "5\+3"), ("7**2", "7\*\*2"), ("(5^2)", "\(5\^2\)"))
+
 TOKENIZE_TEST = (
-    {"arg": "1+1", "res": [1, "+", 1]},
-    {"arg": "1-1", "res": [1, "-", 1]},
-    {"arg": "-1+1", "res": [-1, "+", 1]},
-    {"arg": "-1-1", "res": [-1, "-", 1]},
-    {"arg": "2*-1", "res": [2, "*", -1]},
+    ("1+1", [1, "+", 1]),
+    ("1-1", [1, "-", 1]),
+    ("-1+1", [-1, "+", 1]),
+    ("-1-1", [-1, "-", 1]),
+    ("2*-1", [2, "*", -1]),
 )
+
+INFIX2RPN_TEST = (([2, "+", 3], [2, 3, "+"]),)
+
 
 CALCULATE_TEST = (
-    {"arg": "1+1", "res": 2},
-    {"arg": "1-1", "res": 0},
-    {"arg": "-1+1", "res": 0},
-    {"arg": "-1-1", "res": -2},
-    {"arg": "2*-1", "res": -2},
-    {"arg": "1/0", "res": inf},
-    {"arg": "10j", "res": 10j},
-    {"arg": "10e5", "res": 10e5},
-    {"arg": "sin(3.14)", "res": sin(3.14)},
+    ("1+8", 9),
+    ("1-5", -4),
+    ("2*-1", -2),
+    ("10j/50j", 10j / 50j),
+    ("10e5**2", 10e5**2),
+    ("2*sin(3.14)-74", 2 * sin(3.14) - 74),
 )
 
+
 class TestCase(unittest.TestCase):
+    def test_str2digit(self):
+        for pair in STR2DIGIT_TEST:
+            self.assertEqual(str2digit(pair[0]), pair[1])
+
+    def test_sanitaze(self):
+        for pair in SANITAZE_TEST:
+            self.assertEqual(sanitaze(pair[0]), pair[1])
+
     def test_tokenize(self):
         for pair in TOKENIZE_TEST:
-            self.assertEqual(tokenize(pair["arg"]), pair["res"])
-    
+            self.assertEqual(tokenize(pair[0]), pair[1])
+
+    def test_infix2rpn(self):
+        for pair in INFIX2RPN_TEST:
+            self.assertEqual(infix2rpn(pair[0]), pair[1])
+
     def test_calculate(self):
         for pair in CALCULATE_TEST:
-            self.assertEqual(calculate(pair["arg"]), pair["res"])
+            self.assertEqual(calculate(pair[0]), pair[1])

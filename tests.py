9c85c58cd81c0f251ea9e059450853f36d3f4a4b
TheYahton <>
2024-06-11 11:48:59 +0000

    фикс: тесты

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
-from main import str2digit, sanitaze, tokenize, infix2rpn, evaluate_rpn, calculate
+from main import str2digit, sanitaze, tokenize, parse, interprete, calculate
 from math import inf, sin
 
 STR2DIGIT_TEST = (("5", 5), ("-6", -6), ("3.65", 3.65), ("3+5.5j", 3 + 5.5j))
 
 SANITAZE_TEST = (("5+3", "5\+3"), ("7**2", "7\*\*2"), ("(5^2)", "\(5\^2\)"))
 
-TOKENIZE_TEST = (
+LEXER_TEST = (
     ("1+1", [1, "+", 1]),
     ("1-1", [1, "-", 1]),
     ("-1+1", [-1, "+", 1]),
@@ -14,7 +14,7 @@ TOKENIZE_TEST = (
     ("2*-1", [2, "*", -1]),
 )
 
-INFIX2RPN_TEST = (([2, "+", 3], [2, 3, "+"]),)
+PARSER_TEST = (([2, "+", 3], [2, 3, "+"]),)
 
 
 CALCULATE_TEST = (
@@ -36,13 +36,13 @@ class TestCase(unittest.TestCase):
         for pair in SANITAZE_TEST:
             self.assertEqual(sanitaze(pair[0]), pair[1])
 
-    def test_tokenize(self):
-        for pair in TOKENIZE_TEST:
+    def test_lexer(self):
+        for pair in LEXER_TEST:
             self.assertEqual(tokenize(pair[0]), pair[1])
 
-    def test_infix2rpn(self):
-        for pair in INFIX2RPN_TEST:
-            self.assertEqual(infix2rpn(pair[0]), pair[1])
+    def test_parser(self):
+        for pair in PARSER_TEST:
+            self.assertEqual(parse(pair[0]), pair[1])
 
     def test_calculate(self):
         for pair in CALCULATE_TEST:
